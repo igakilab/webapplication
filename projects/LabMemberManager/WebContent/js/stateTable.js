a41b4348f8ad26d0f07b874608b484d2fe408f26
kioke takaaki <taka.vao.321@gmail.com>
2015-09-30 23:41:23 +0900

    スクリプトを少し変更
    列の指定を簡単にできるような関数を用意
    ○を中央に表示

--- projects/LabMemberManager/WebContent/js/stateTable.js
+++ projects/LabMemberManager/WebContent/js/stateTable.js
@@ -1,37 +1,46 @@
-var StateTable = {
+var StateTable = {};
 
-		states : [101, 103, 104, 102],
+StateTable.states = [101, 103, 104, 102];
+StateTable.defaultColumn = 3;
 
-		appendTdElements: function(dest, pos, len, val){
-			for(var i=0; i<len; i++){
-				if( i == pos ){
-					dest.append($("<td></td>").append(val));
-				}else{
-					dest.append($("<td></td>"));
-				}
-			}
-		},
-
-		appendRow: function(id, name, inf){
-			var row = $("#" + id);
-			var position = StateTable.getPosition(inf.stateCode);
-			row.empty();
-			row.append($("<td></td>").text(name));
-			StateTable.appendTdElements(row, position, StateTable.states.length, "●");
-		},
-
-		getPosition: function(code){
-			for(var i=0; i<StateTable.states.length; i++){
-				if( code == StateTable.states[i] ){
-					return i;
-				}
-			}
-			return 3;
-		},
-
-		setStateRow: function(name, id){
-			LmmManager.getMemberState(name, { callback: function(st){
-				StateTable.appendRow(id, name, st);
-			}});
+StateTable.appendTdElements = function(dest, pos, len, val){
+	for(var i=0; i<len; i++){
+		var newtd = $("<td></td>");
+		newtd.attr("style", "text-align:center");
+		if( i == pos ){
+			dest.append(newtd.append(val));
+		}else{
+			dest.append(newtd);
 		}
+	}
+};
+
+StateTable.appendRow = function(id, name, inf){
+	var row = $("#" + id);
+	var position = this.getPosition(inf.stateCode);
+	row.empty();
+	row.append($("<td></td>").text(name));
+	this.appendTdElements(row, position, this.states.length, "●");
+};
+
+StateTable.getPosition = function(code){
+	for(var i=0; i<this.states.length; i++){
+		if( code == this.states[i] ){
+			return i;
+		}
+	}
+	return this.defaultColumn;
+};
+
+StateTable.setStateRow = function(name, id){
+	LmmManager.getMemberState(name, { callback: function(st){
+		StateTable.appendRow(id, name, st);
+	}});
+};
+
+StateTable.setColumn = function(args){
+	this.states = new Array();
+	for(var i=0; i<arguments.length; i++){
+		states[i] = arguments[i];
+	}
 };
