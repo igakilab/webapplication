403bfe6fe1d7641160751624b559afd5385aac82
kioke takaaki <taka.vao.321@gmail.com>
2015-10-15 00:29:15 +0900

    HistoryStatisticsクラスを追加
    履歴分析クラスを追加
    テストページのリンクを動的に生成するように変更

---
+++ projects/LabMemberManager/WebContent/test/testpagelink.js
@@ -0,0 +1,17 @@
+var TestPageLink = {};
+
+TestPageLink.links = [
+    {text:"lmmtest.html", href:"lmmtest.html"},
+    {text:"personalDate.html", href:"personalData.html"},
+    {text:"records.html", href:"records.html"}
+]
+
+TestPageLink.appendToList = function(jq_elem){
+	var new_elem;
+	for(var i=0; i<this.links.length; i++){
+		new_elem = $("<a></a>");
+		new_elem.attr("href", this.links[i].href);
+		new_elem.text(this.links[i].text);
+		jq_elem.append($("<li></li>").append(new_elem));
+	}
+}
