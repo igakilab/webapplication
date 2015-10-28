069efe3a4987eb534c694bbbb074d005f7be03a4
Kitabayashi <e1b13038@gmail.com>
2015-10-28 13:41:26 +0900

    ガントとりあえずできた

--- projects/LabMemberManager/WebContent/js/ganttManager.js
+++ projects/LabMemberManager/WebContent/js/ganttManager.js
@@ -5,7 +5,8 @@ GanttManager.defaultCustomClass = "ganttOrange";
 GanttManager.defaultProfile = {
 	source: [],
 	navigate: "scroll",
-	maxScale: "hours",
+	maxScale: "month",
+	minScale: "hours",
 	itemsPerPage: 10,
 	months : ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
 	dow : ["日", "月", "火", "水", "木", "金", "土"],
